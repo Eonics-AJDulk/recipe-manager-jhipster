8fa7d827236cf06ccdbc9abfbe82408b1d1d70bb
Antony den Dulk <antony.dendulk@eonics.nl>
2024-02-01 18:24:24 +0100

    Added RecipeWithIngredientsDTOTest

---
+++ src/test/java/nl/eonics/recipemanager/custom/RecipeWithIngredientsDTOTest.java
@@ -0,0 +1,69 @@
+package nl.eonics.recipemanager.custom;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import nl.eonics.recipemanager.service.dto.IngredientDTO;
+import nl.eonics.recipemanager.service.dto.RecipeDTO;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains unit tests for the RecipeWithIngredientsDTO class.
+ */
+public class RecipeWithIngredientsDTOTest {
+
+    /**
+     * This test checks that the getRecipe method of the RecipeWithIngredientsDTO class
+     * returns the correct RecipeDTO object.
+     */
+    @Test
+    public void getRecipeReturnsCorrectRecipe() {
+        // Given
+        RecipeDTO recipeDTO = new RecipeDTO();
+        List<IngredientDTO> ingredients = List.of(new IngredientDTO());
+        RecipeWithIngredientsDTO recipeWithIngredientsDTO = new RecipeWithIngredientsDTO(recipeDTO, ingredients);
+
+        // When
+        RecipeDTO actual = recipeWithIngredientsDTO.getRecipe();
+
+        // Then
+        assertEquals(recipeDTO, actual);
+    }
+
+    /**
+     * This test checks that the getIngredients method of the RecipeWithIngredientsDTO class
+     * returns the correct list of IngredientDTO objects.
+     */
+    @Test
+    public void getIngredientsReturnsCorrectIngredients() {
+        // Given
+        RecipeDTO recipeDTO = new RecipeDTO();
+        List<IngredientDTO> ingredients = List.of(new IngredientDTO());
+        RecipeWithIngredientsDTO recipeWithIngredientsDTO = new RecipeWithIngredientsDTO(recipeDTO, ingredients);
+
+        // When
+        List<IngredientDTO> actual = recipeWithIngredientsDTO.getIngredients();
+
+        // Then
+        assertEquals(ingredients, actual);
+    }
+
+    /**
+     * This test checks that the getIngredients method of the RecipeWithIngredientsDTO class
+     * returns an empty list when no ingredients are provided.
+     */
+    @Test
+    public void getIngredientsReturnsEmptyListWhenNoIngredientsProvided() {
+        // Given
+        RecipeDTO recipeDTO = new RecipeDTO();
+        RecipeWithIngredientsDTO recipeWithIngredientsDTO = new RecipeWithIngredientsDTO(recipeDTO, Collections.emptyList());
+
+        // When
+        List<IngredientDTO> actual = recipeWithIngredientsDTO.getIngredients();
+
+        // Then
+        assertEquals(Collections.emptyList(), actual);
+    }
+}
